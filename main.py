636aaa3d4bb0363516f859c5f494e1f043413c01
zora89 <42845567+zora89@users.noreply.github.com>
2021-09-03 21:58:15 +0000

    DB working

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ import iss
 from flask import Flask, render_template, flash
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, FloatField, PasswordField, BooleanField
+from wtforms import StringField, SubmitField, FloatField, PasswordField, BooleanField, IntegerField
 from wtforms.fields.html5 import EmailField
 from wtforms.validators import DataRequired, Email
 from replit import db
 from weathers import weather
 from flight import flight_message
+import requests
 
 my_secret = os.environ['SECRET_KEY']
 id_key = 0
@@ -19,11 +20,11 @@ app.config['SECRET_KEY'] = my_secret
 Bootstrap(app)
 
 class first_user_form(FlaskForm):
-	name = StringField('Full Name', validators=[DataRequired()])
-	email = EmailField('Email Address', validators=[DataRequired(), Email()])
-	password = PasswordField('Password', validators=[DataRequired()])
-	remember_me = BooleanField('Remember me')
-	submit = SubmitField('Submit !')
+  name = StringField('Full Name', validators=[DataRequired()])
+  email = EmailField('Email Address', validators=[DataRequired(), Email()])
+  password = PasswordField('Password', validators=[DataRequired()])
+  remember_me = BooleanField('Remember me')
+  submit = SubmitField('Submit !')
 
 class iss_form(FlaskForm):
 	name = StringField("Name a Planet ? ", validators=[DataRequired()])
@@ -49,27 +50,29 @@ def index():
 
 @app.route('/user', methods=['GET', 'POST'])
 def user():
-	name = None
-	form = first_user_form()
-	user = []
-	user.append(db["user"])
-
-	email = []
-	email.append(db["email"])
-
-	if form.validate_on_submit():
-
-		name = form.name.data
-		user.append(name)
-		db["user"] = user
-		form.name.data = ''
-
-		email_id = form.email.data
-		email.append(email_id)
-		db["email"] = email
-		form.email.data = ''
-		flash("Cool, you're signed up ")
-	return render_template('user.html', name=name, form=form)
+  name = None
+  form = first_user_form()
+  user = []
+  user.append(db["user"])
+  password=[]
+  password.append(db['password'])
+  email = []
+  email.append(db["email"])
+  if form.validate_on_submit():
+    name = form.name.data
+    user.append(name)
+    db["user"] = user
+    form.name.data = ''
+    email_id = form.email.data
+    email.append(email_id)
+    db["email"] = email
+    form.email.data = ''
+    passw = form.password.data
+    password.append(passw)
+    db['password'] = password
+    form.password.data = ''
+    flash("Cool, you're signed up ")
+  return render_template('user.html', name = name, form = form)
 
 @app.route('/iss', methods=['GET', 'POST'])
 def iss_page():
@@ -138,4 +141,4 @@ def weathery():
   return render_template('weather.html',name=name,form=form)
 
 if __name__ == '__main__':
-	app.run(debug=False, host='0.0.0.0', port=8080)
+	app.run(debug=True, host='0.0.0.0', port=8080)
