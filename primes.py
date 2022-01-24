617e3be092614b4dc13fe7e6f56f821541961bf9
zora89 <42845567+zora89@users.noreply.github.com>
2022-01-24 19:29:05 +0000

    got prime running with pass, and while loop limited to an int.

--- primes.py
+++ primes.py
@@ -2,25 +2,28 @@ def prime():
   a=1
   b=1
   flag = None
+  list = []
 
-  while True:
+  while a != 1001:
     
     if a == 1:
       flag = False
-      break
+      pass
     else:
       if a > 1:
-        for i in range(2,a):
+        for i in range(2,1000):
           if (a % i) == 0:
             flag = True
-            break
+            pass
     if flag:
       mes = f"{a} is not a Prime."
+      list.append(mes)
       flag = None
       a += b
     else:
       mes = f"{a} is PRIME."
+      list.append(mes)
       flag = None
       a += b
-    print(mes)
-    return mes
+    print('Running Prime')
+  return list
