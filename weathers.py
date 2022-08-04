541c3e157b782e4be2e1b7cb5f2a266596381dab
Zorawar Purohit <42845567+zora89@users.noreply.github.com>
2022-08-04 20:40:06 +0000

    hindi weather nearing

--- weathers.py
+++ weathers.py
@@ -3,21 +3,30 @@ import os
 import googlemaps
 
 
-
-
 def weather(city):
-  #r2 = requests.get(
-    #f'https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key={os.environ["googlemap_key"]}')
-  gmaps = googlemaps.Client(key=os.environ['googlemap_key'])
-  geocode_result = gmaps.geocode(city)
-  print(geocode_result)
-  r = requests.get(
-    f"https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid={os.environ['openweather_key']}"
-  )
-  r = r.json()
-  #r2 = r2.json()
-  print("R is responding with: :", r)
-  #print("R2 is responding with: :", r2)
+    r2 = requests.get(
+        f'https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key={os.environ["googlemap_key"]}'
+    )
+    #gmaps = googlemaps.Client(key=os.environ['googlemap_key'])
+    #geocode_result = gmaps.geocode(city)
+    #print(geocode_result)
+    r = requests.get(
+        f"https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&exclude=hourly,minutely,pressure,uvi,icon,wind_deg&lang=hi&units=metric&appid={os.environ['openweather_key']}"
+    )
+    r = r.json()
+    r2 = r2.json()
+    print("R is responding with: :", r)
+    print("")
+    print("")
+    print("R2 is responding with: :", r2)
+    print("")
+    print("")
+    
+  
+    format = r['current']
+    format2 = format['temp']
+    format3 = format['weather']
+    print(f'formatted is {format2}, {format3}')
 
 
 #  data_refined = f'Temperature °C = {data["temp"]} | Feels Like =
