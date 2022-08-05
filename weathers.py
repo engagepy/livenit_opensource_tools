39c29acf845b7a93e9ef32731030e4deecb9e3ad
Zorawar Purohit <42845567+zora89@users.noreply.github.com>
2022-08-05 17:05:20 +0000

    geocoding working

--- weathers.py
+++ weathers.py
@@ -4,9 +4,9 @@ import googlemaps
 
 
 def weather(lat, lon):
-    """r2 = requests.get(
-        f'https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key={os.environ["googlemap_key"]}'
-    )"""
+    r2 = requests.get(
+        f'https://maps.googleapis.com/maps/api/geocode/json?address=Port+Blair, India&key={os.environ["googlemap_key"]}'
+    )
     #gmaps = googlemaps.Client(key=os.environ['googlemap_key'])
     #geocode_result = gmaps.geocode(city)
     #print(geocode_result)
@@ -14,8 +14,8 @@ def weather(lat, lon):
         f"https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude=hourly,minutely,pressure,uvi,icon,wind_deg&lang=hi&units=metric&appid={os.environ['openweather_key']}"
     )
     r = r.json()
-    #r2 = r2.json()
-    
+    r2 = r2.json()
+    print(r2)
   
     format = r['current']
     formati = r['timezone']
@@ -25,7 +25,7 @@ def weather(lat, lon):
     print(f'formatted is {format} \n {format2} \n {format3} \n \n \n {formati}')
 
 
-    data_refined = f'Weather at Latitude: {lat} - Longitude: {lon} in TimeZone = {formati} ||\n Temp = {format2}°C | \n Status = {format4} |\n Hindi = {format3}'
+    data_refined = f'Weather @ Latitude: {lat} & Longitude: {lon} in TimeZone : {formati} ||\n Temp = {format2}°C | \n Status = {format4} |\n Hindi = {format3}'
     data_refined = str(data_refined)
     #print(data_refined)
     return data_refined
