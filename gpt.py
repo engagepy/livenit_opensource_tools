3d1d74d5406620964a58be4e1ac1e6735f8910bd
engagepy <42845567+engagepy@users.noreply.github.com>
2023-02-13 20:11:38 +0000

    landing tweaked

--- gpt.py
+++ gpt.py
@@ -13,7 +13,7 @@ def generate(prompt):
       model="text-davinci-003",
       prompt=start_sequence,
       temperature=0,
-      max_tokens=512,
+      max_tokens=1111,
       top_p=1,
       frequency_penalty=0.51,
       presence_penalty=0.5,
